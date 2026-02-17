3e89bf5f1aa99e8080e7fa228f2c20d007b92d92
Andreas <ga.nechita@gmail.com>
2026-02-17 20:18:45 +0100

    server.js -> added functionality for groups fetch

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,18 @@ const __dirname = path.dirname(__filename);
 
 // /words Route liest die JSON-Datei
 app.get("/words", (req, res) => {
-  const filePath = path.join(__dirname, "data", "words.json");
+  const group = req.query.group;
+  const filePath = path.join(__dirname, "data", `${group}.json`);
 
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      console.error("Fehler beim Lesen der Datei:", err);
-      return res.status(500).json({ error: "Datei konnte nicht gelesen werden" });
+      return res.status(404).json({ error: "Datei nicht gefunden" });
     }
+
     try {
       const words = JSON.parse(data);
       res.json(words);
     } catch (parseErr) {
-      console.error("JSON konnte nicht geparst werden:", parseErr);
       res.status(500).json({ error: "Ungültiges JSON" });
     }
   });
