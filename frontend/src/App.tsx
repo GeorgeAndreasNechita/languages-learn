d639793fe883523ca01c7537de2443a5c7d3625e
Andreas <ga.nechita@gmail.com>
2026-02-18 11:01:45 +0100

    router added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+// App.tsx
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Words from "./Words";
+import Articles from "./Articles";
+import Settings from "./Settings";
+import "./App.css";
+
+export default function App() {
+  return (
+    <Router>
+      <nav className="tab-nav">
+        <Link to="/">Words</Link>
+        <Link to="/articles">Articles</Link>
+        <Link to="/settings">Einstellungen</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Words />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ frontend/src/Settings.tsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+  return (
+    <div>
+      <h1>Einstellungen</h1>
+      <p>Hier kann man Dinge ändern.</p>
+    </div>
+  );
+}
